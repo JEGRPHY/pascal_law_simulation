83aae074e2fc8fd2ca46f62d37e496a0781f1924
JEGRPHY <jegrjebar@gmail.com>
2024-11-21 08:58:03 +0300

    Delete utils directory

--- utils/visualization.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def render_3d_model(animal):
-    """
-    Display a placeholder for rendering a 3D model.
-    In future versions, integrate actual 3D models.
-    """
-    st.write(f"Rendering 3D model of {animal}... (placeholder)")
-
