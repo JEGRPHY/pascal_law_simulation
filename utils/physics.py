a5a521058f97fb376eb1fb4d6c079fc95986f545
JEGRPHY <jegrjebar@gmail.com>
2024-11-21 09:04:11 +0300

    Update physics.py

--- utils/physics.py
+++ utils/physics.py
@@ -1,1 +1,9 @@
+def calculate_pressure(force, area):
+    """
+    Calculate pressure using Pascal's Law.
+    :param force: Applied force (N)
+    :param area: Cross-sectional area (m^2)
+    :return: Pressure (Pa)
+    """
+    return force / area
 
